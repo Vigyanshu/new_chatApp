4d8a5ae6428a4e51009ad45706cd8ebcd3ec4775
Vigyanshu <P.Vigyanshu217@gmail.com>
2024-06-02 23:29:02 +0530

    Commit new changes

--- chatapp/chat/urls.py
+++ chatapp/chat/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path("", chat_views.chatPage, name = "chat-page"),
+    path("", chat_views.chatPage, name="chat-page"),
 
-    #Login section
+    # login-section
     path("auth/login/", LoginView.as_view
          (template_name="chat/LoginPage.html"), name="login-user"),
     path("auth/logout/", LogoutView.as_view(), name="logout-user"),
--- chatapp/chatapp/asgi.py
+++ chatapp/chatapp/asgi.py
@@ -7,10 +7,34 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-import os
+"""import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
+django_asgi_application = get_asgi_application()
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatapp.settings")
+application = ProtocolTypeRouter({
+    "http": django_asgi_application
+})
+
+ASGI_APPLICATION = 'chatapp.asgi.application'"""
+import os
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTING_MODULE','chatapp.settings')
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat import routing
+
+application = ProtocolTypeRouter(
+    {
+        "http":get_asgi_application(),
+        "websocket":AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
+    }
+)
